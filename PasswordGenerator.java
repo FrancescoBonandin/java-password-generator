133cf329e5fe4c5533187601d242cb82f92c3416
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-11-09 16:51:16 +0100

    minor refactoring

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -2,22 +2,10 @@ package org.lessons.java.security;
 
 import java.util.Scanner;
 
-//Todo
-//Create un nuovo progetto java chiamato java-password-generator.
-//Aggiungete una classe PasswordGenerator nel package org.lessons.java.security che contiene un metodo main.
-//Il programma deve fare quanto segue:
-//
-//salvare in opportune variabili il nome, cognome, colore preferito e data di nascita di un utente suddivisa in giorno, mese e anno in numero
-//generare (e stampare a video) una password concatenando nome, cognome, colore preferito e somma di giorno, mese e anno di nascita, separate dal carattere -
-//Esempio
-//Ho un utente che si chiama Pinco Pallo, nato il 12/05/1994, il cui colore preferito è il magenta.
-//La sua password sarà Pinco-Pallo-magenta-2011
 
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		
-		String password = "" ;
 		
 		Scanner in = new Scanner(System.in) ;
 		
@@ -45,7 +33,7 @@ public class PasswordGenerator {
 		
 		int dateSum = day+month+year;
 		
-		password = name + "-" + surname + "-" + color + "-" + dateSum;
+		String password = name + "-" + surname + "-" + color + "-" + dateSum;
 		System.out.println("la tua password è : " + password);
 	}
 
