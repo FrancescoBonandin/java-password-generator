e27ccdc5443e16a9f30cb37c2b50ebe53282f5e1
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-11-09 16:48:13 +0100

    pwd generator v0.0.1

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -17,8 +17,36 @@ public class PasswordGenerator {
 
 	public static void main(String[] args) {
 		
+		String password = "" ;
+		
 		Scanner in = new Scanner(System.in) ;
+		
+		System.out.println("dimmi il tuo nome?");
+		String name = in.nextLine();
+		
+		System.out.println("dimmi il tuo cognome?");
+		String surname = in.nextLine();
+		
+		System.out.println("dimmi il tuo colore preferito?");
+		String color = in.nextLine();
+		
+		System.out.println("dimmi il tuo giorno di nascita?");
+		String dayOfBirth =  in.nextLine();
+		int day = Integer.valueOf(dayOfBirth);
+		
+		System.out.println("dimmi il tuo mese di nascita?");
+		String monthOfBirth = in.nextLine();
+		int month = Integer.valueOf(monthOfBirth);
+		
+		System.out.println("dimmi il tuo anno di nascita?");
+		String yearOfBirth = in.nextLine();
+		int year = Integer.valueOf(yearOfBirth);
 
+		
+		int dateSum = day+month+year;
+		
+		password = name + "-" + surname + "-" + color + "-" + dateSum;
+		System.out.println("la tua password è : " + password);
 	}
 
 }
